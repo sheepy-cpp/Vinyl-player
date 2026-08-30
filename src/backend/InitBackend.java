f4eaaa7b0a5855b0ef37a35035f74c2ec016351a
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 22:48:38 +0200

    updated it to match fetch settings and added debug stuff

--- src/backend/InitBackend.java
+++ src/backend/InitBackend.java
@@ -6,15 +6,22 @@ import java.nio.file.Path;
 
 public class InitBackend {
     public static void InitB() throws IOException, URISyntaxException {
-        Path songsPath = InitSongDir.ensureSongsFolder(); // fetches /songs directory and creates it if not available
+        boolean exists = InitSongDir.songsFolderExists(); // checks if /songs exists
+        Path songsPath = InitSongDir.ensureSongsFolder(); // fetches path to /songs
 
-
-        FetchSettings.checkAndCreateSettings();
-        int debugMode = FetchSettings.readSettings();
+        FetchSettings.checkAndCreateSettings();           // checks if settings json file exists
+        int debugMode = FetchSettings.readSettings();     // reads out debug mode
 
         if (debugMode == 1){
-            System.out.println("debug is on");
-            System.out.println(songsPath);
+            System.out.println("debug is on"); 
+            // debug is off per default so theres no debug where it gets created or if it exists
+
+            if (exists){
+                System.out.println("songs directory is at: " + songsPath);
+            }else{
+                System.out.println("songs directory got created at: " + songsPath);
+            }
         }
+
     }
 }
