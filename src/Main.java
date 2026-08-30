bc1e2b914bff5d2a0353630eb6a5bf205e7fd743
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 22:20:45 +0200

    removed unused import (org.json.*)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import backend.InitBackend;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import org.json.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
