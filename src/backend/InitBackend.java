03ced6782e96c4b2e6e2f67a923267c1d6b4a620
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 22:12:56 +0200

    creates settings if needed and reads debug mode (language is still WIP)

--- src/backend/InitBackend.java
+++ src/backend/InitBackend.java
@@ -5,9 +5,16 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 
 public class InitBackend {
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void InitB() throws IOException, URISyntaxException {
         Path songsPath = InitSongDir.ensureSongsFolder(); // fetches /songs directory and creates it if not available
 
-        System.out.println(songsPath);
+
+        FetchSettings.checkAndCreateSettings();
+        int debugMode = FetchSettings.readSettings();
+
+        if (debugMode == 1){
+            System.out.println("debug is on");
+            System.out.println(songsPath);
+        }
     }
 }
