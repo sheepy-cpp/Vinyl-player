b668a388fbb9e0788acf5a921f716fcbc897835e
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 21:43:04 +0200

    made Initbackend prettier

--- src/backend/InitBackend.java
+++ src/backend/InitBackend.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 
-
 public class InitBackend {
     public static void main(String[] args) throws IOException, URISyntaxException {
         Path songsPath = InitSongDir.ensureSongsFolder(); // fetches /songs directory and creates it if not available
