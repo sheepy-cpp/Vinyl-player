8b9391031688aa317d72745d2f34d73c2b5cce8b
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 22:47:57 +0200

    two functions if it exists or not -> needed for debug

--- src/backend/InitSongDir.java
+++ src/backend/InitSongDir.java
@@ -8,11 +8,16 @@ import customlib.FindProjectRoot;
 
 public class InitSongDir {
 
-    public static Path ensureSongsFolder() throws IOException, URISyntaxException {
+    public static boolean songsFolderExists() throws IOException, URISyntaxException {
         Path projectRoot = FindProjectRoot.findProjectRoot();
-        Path songsPath = projectRoot.resolve("songs");  // this finds the path to the songs directory using findProjectRoot()
-        Files.createDirectories(songsPath);
-        return songsPath;
+        Path songsPath = projectRoot.resolve("songs"); // this finds the path to the songs directory using findProjectRoot()
+        return Files.exists(songsPath);
     }
 
+    public static Path ensureSongsFolder() throws IOException, URISyntaxException {
+        Path projectRoot = FindProjectRoot.findProjectRoot();
+        Path songsPath = projectRoot.resolve("songs");
+        Files.createDirectories(songsPath); // Creates songs dir if needed
+        return songsPath;
+    }
 }
