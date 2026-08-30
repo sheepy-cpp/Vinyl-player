1adc2e5c0b9db88fd2abd1b00a654692175756d3
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 21:10:59 +0200

    importet org.json

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import backend.FetchSettings;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import org.json.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
