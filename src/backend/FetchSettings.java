8db8df46432e44324abeb171b2cc13f4c7bd5cb0
sheepy-cpp <pixelsheep3000mail@gmail.com>
2026-08-30 22:11:46 +0200

    parses debug mode value into boolean

--- src/backend/FetchSettings.java
+++ src/backend/FetchSettings.java
@@ -1,11 +1,15 @@
 package backend;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import customlib.FindProjectRoot;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 
 public class FetchSettings {
 
@@ -36,6 +40,27 @@ public class FetchSettings {
             }
         } catch (IOException | URISyntaxException e) {
             System.err.println("error creating " + FILE_NAME + ": " + e.getMessage());
+        }
+    }
+
+    public static int readSettings(){
+        try {
+            Path projectRoot = FindProjectRoot.findProjectRoot();
+            Path settingsPath = projectRoot.resolve(FILE_NAME);
+            File file = settingsPath.toFile();
+
+            try (FileReader reader = new FileReader(file)) {
+                JSONObject json = new JSONObject(new JSONTokener(reader));
+
+                String debugModeStr = json.getString("debugMode");
+                boolean debugMode = Boolean.parseBoolean(debugModeStr);
+
+                return debugMode ? 1 : 0;
+            }
+
+        } catch (IOException | URISyntaxException e) {
+            System.err.println("error reading " + FILE_NAME + ": " + e.getMessage());
+            return -1; 
         }
     }
 
